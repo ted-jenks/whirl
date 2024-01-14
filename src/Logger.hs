f78b7768fc9cbb071a77c09794a85bc4a034a95a
Ted Jenks <ej3718@ic.ac.uk>
2024-01-14 15:40:55 +0000

    log filter actually work and test more thorough

--- src/Logger.hs
+++ src/Logger.hs
@@ -6,6 +6,8 @@ module Logger
   , initLogger
   , logMessage
   , waitLogger
+  , logQueue
+  , logLevel
   ) where
 
 import Control.Concurrent.Async (async, wait)
@@ -13,7 +15,23 @@ import Control.Concurrent.STM
 import Control.Monad (forever, when)
 
 -- | The severity levels for log messages.
-data LogLevel = DEBUG | INFO | WARNING | ERROR  deriving (Eq, Ord, Show)
+data LogLevel = DEBUG | INFO | WARNING | ERROR deriving (Eq, Show)
+
+instance Ord LogLevel where
+  compare DEBUG   DEBUG   = EQ
+  compare DEBUG   _       = LT
+  compare INFO    INFO    = EQ
+  compare INFO    DEBUG   = GT
+  compare INFO    _       = LT
+  compare WARNING WARNING = EQ
+  compare WARNING DEBUG   = GT
+  compare WARNING INFO    = GT
+  compare WARNING _       = LT
+  compare ERROR   ERROR   = EQ
+  compare ERROR   DEBUG   = GT
+  compare ERROR   INFO    = GT
+  compare ERROR   WARNING = GT
+
 
 -- | Represents a log message with a severity level and a message.
 data LogMessage = LogMessage
@@ -71,8 +89,7 @@ initLogger config = do
 logWorker :: Logger -> IO ()
 logWorker logger = forever $ do
   msg <- atomically $ readTQueue (logQueue logger)
-  when (logConfigLevel (logConfig logger) <= logLevel msg) $
-    logCallback (logConfig logger) msg
+  logCallback (logConfig logger) msg
 
 -- | Logs a message with the specified severity level.
 --
@@ -89,8 +106,11 @@ logWorker logger = forever $ do
 -- In this case, the debug message won't be logged because the default
 -- configuration has a log level of INFO, so DEBUG messages are ignored.
 logMessage :: Logger -> LogLevel -> String -> IO ()
-logMessage logger level msg =
-  atomically $ writeTQueue (logQueue logger) (LogMessage level msg)
+logMessage logger level msg = do
+  let configLevel = logConfigLevel (logConfig logger)
+  when (configLevel <= level) $
+    atomically $ writeTQueue (logQueue logger) (LogMessage level msg)
+
 
 -- | Helper function to wait for a logger to finish
 waitLogger :: Logger -> IO ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,7 @@
 import Test.Tasty
 import Test.Tasty.HUnit
 import Logger
-import Control.Concurrent.Async (async, wait)
+import Control.Concurrent.STM
 
 main :: IO ()
 main = defaultMain test
@@ -18,14 +18,38 @@ loggerTests =
         logMessage logger INFO "This is an informational message."
         logMessage logger WARNING "This is a warning message."
         logMessage logger ERROR "This is an error message."
-        logMessage logger DEBUG "This is a debug message."  -- Won't be logged due to the default log level
-        asyncLogger <- async $ waitLogger logger
-        wait asyncLogger
+        logMessage logger DEBUG "This is a debug message."
+
+        logMessages <- atomically $ flushTQueue (logQueue logger)
+
+        assertEqual "Number of log messages" 3 (length logMessages)
+        assertBool "INFO log message present" (INFO `elem` map logLevel logMessages)
+        assertBool "WARNING log message present" (WARNING `elem` map logLevel logMessages)
+        assertBool "ERROR log message present" (ERROR `elem` map logLevel logMessages)
     , testCase "handles custom log levels and callbacks" $ do
         let customConfig = LoggerConfig DEBUG (\msg -> putStrLn $ "Custom Callback: " ++ show msg)
         customLogger <- initLogger customConfig
+
         logMessage customLogger DEBUG "This is a debug message."
         logMessage customLogger INFO "This is an informational message."
-        asyncCustomLogger <- async $ waitLogger customLogger
-        wait asyncCustomLogger
+
+        logMessages <- atomically $ flushTQueue (logQueue customLogger)
+
+        assertEqual "Number of log messages" 2 (length logMessages)
+        assertBool "DEBUG log message present" (DEBUG `elem` map logLevel logMessages)
+        assertBool "INFO log message present" (INFO `elem` map logLevel logMessages)
+    , testCase "logs messages with the correct severity level (Warning)" $ do
+        let customConfig = LoggerConfig WARNING (\msg -> putStrLn $ "Custom Callback: " ++ show msg)
+        customLogger <- initLogger customConfig
+
+        logMessage customLogger DEBUG "This is a debug message."  -- Won't be logged due to the custom log level
+        logMessage customLogger INFO "This is an informational message."  -- Won't be logged due to the custom log level
+        logMessage customLogger WARNING "This is a warning message."
+        logMessage customLogger ERROR "This is an error message."
+
+        logMessages <- atomically $ flushTQueue (logQueue customLogger)
+
+        assertEqual "Number of log messages" 2 (length logMessages)
+        assertBool "WARNING log message present" (WARNING `elem` map logLevel logMessages)
+        assertBool "ERROR log message present" (ERROR `elem` map logLevel logMessages)
     ]
